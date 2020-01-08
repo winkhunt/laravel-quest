e3f85b58b26080a1f74b8c28f84ff6a08ca814b3
Win Khunt <winkhunt007@gmail.com>
2020-01-08 16:30:33 +0630

    add vote answer view

--- app/Question.php
+++ app/Question.php
@@ -25,4 +25,16 @@ class Question extends Model
     {
         return $this->created_at->diffForHumans();
     }
+    public function getStatusAttribute()
+    {
+        if($this->answers > 0)
+        {
+            if($this->best_ans_id)
+            {
+                return "answered_accepted";
+            }
+            return "answered";
+        }
+        return "unanswered";
+    }
 }
