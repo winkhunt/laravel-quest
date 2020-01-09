e821a8cc3e2197994f51d80764d6f204f6cae6de
Win Khunt <winkhunt007@gmail.com>
2020-01-09 12:14:35 +0630

    create form

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -30,7 +30,8 @@ class QuestionsController extends Controller
      */
     public function create()
     {
-        //
+        $question =new Question();
+        return view('questions.create',\compact('question'));
     }
 
     /**
---
+++ resources/views/questions/create.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex align-items-center">
+                        <h2>Ask Questions</h2>
+                        <div class="ml-auto">
+                            <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back To Home</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                     <form action="{{ route('questions.store') }}" action="post">
+                        <div class="form-group">
+                            <label for="title">Title</label>
+                            <input type="text" class="form-control {{ $errors->has('title') ? 'is-invalid' : ''}}" name="title">
+                            @if ($errors->has('title'))
+                                <div class="invalid-feedback">
+                                    <strong>{{ $errors->first('title') }}</strong>
+                                </div>
+                            @endif
+                        </div>
+                        <div class="form-group">
+                            <label for="body">Describe Detail</label>
+                           <textarea class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}" name="body" rows="5"></textarea>
+                           @if ($errors->has('body'))
+                                <div class="invalid-feedback">
+                                    <strong>{{ $errors->first('body') }}</strong>
+                                </div>
+                            @endif
+                        </div>
+                        <div class="form-group">
+                           
+                           <button type="submit" class="btn btn-outline-primary btn-lg">Ask Question</button>
+                        </div>
+                     </form>
+                   
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
